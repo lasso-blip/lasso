892b9e5b2ad6f5b1293cf108ed6198ea89360acc
lasso-blip <lassomoses@gmail.com>
2022-05-07 08:58:47 +0300

    init

---
+++ about.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <!-- font_awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" >
+
+    <!-- custom-CSS file link  -->
+    <link rel="stylesheet" href="styles.css">
+
+    <!-- swiper-CSS link  -->
+    
+</head>
+<body>
+
+<section class="header">
+
+    <a href="home.php" class="logo">travel.</a>
+    <nav class="navbar">
+        <a href="home.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="package.php">Package</a>
+        <a href="book.php">Book</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+
+<!-- header section ends   -->
+
+<div class="heading" style="background: url('image/nrb.jpg') no-repeat";>
+    <h1>About Us</h1>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+    <div class="box-container">
+        <div class="class-box">
+
+        <h3>quick links</h3>
+        <a href="home.php"><i class="fas fa-angle-right"></i>Home</a>
+        <a href="about.php"><i class="fas fa-angle-right"></i>About</a>
+        <a href="package.php"><i class="fas fa-angle-right"></i>Package</a>
+        <a href="book.php"><i class="fas fa-angle-right"></i>Book</a>
+
+
+        </div>
+
+        <div class="class-box">
+
+        <h3>extra links</h3>
+        <a href="#"><i class="fas fa-angle-right"></i>Ask Questions</a>
+        <a href="#"><i class="fas fa-angle-right"></i>About Us</a>
+        <a href="#"><i class="fas fa-angle-right"></i>Privacy policy</a>
+        <a href="#"><i class="fas fa-angle-right"></i>Terms of Use</a>
+
+
+        </div>
+
+        <div class="class-box">
+
+        <h3>Contact Info</h3>
+        <a href="#"><i class="fas fa-phone"></i>+25474545454</a>
+        <a href="#"><i class="fas fa-phone"></i>+25417777117</a>
+        <a href="#"><i class="fas fa-envelope"></i>lassomoses@gmail.com</a>
+        <a href="#"><i class="fas fa-map"></i>Nairobi, Kenya 00100</a>
+
+
+        </div>
+
+        <div class="class-box">
+
+        <h3>Follow Us</h3>
+        <a href="#"><i class="fab fa-twitter"></i> Twitter</a>
+        <a href="#"><i class="fab fa-facebook"></i> Facebook</a>
+        <a href="#"><i class="fab fa-instagram"></i> Instagram</a>
+        <a href="#"><i class="fab fa-linkedin"></i> LinkedIn</a>
+
+
+        </div>
+
+    </div>
+
+    <div class="credit">created by <span> Mr.Lasso Moses </span> | all rights reserved.</div>
+
+</section>
+<!-- footer section ends  -->
+
+
+
+
+
+
+
+
+
+
+<!-- custom-js file  -->
+    <script src = "script.js"></script>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <!-- font_awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" >
+
+    <!-- custom-CSS file link  -->
+    <link rel="stylesheet" href="styles.css">
+
+    <!-- swiper-CSS link  -->
+    
+</head>
+<body>
+
+<section class="header">
+
+    <a href="home.php" class="logo">travel.</a>
+    <nav class="navbar">
+        <a href="home.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="package.php">Package</a>
+        <a href="book.php">Book</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends  -->
+
+<!-- Home section starts  -->
+
+<section class="home">
+    <div class="home-slider">
+        <div class="W">
+            <div class="slide" style = "background: url('image/zoro-classic (13).jpg') no-repeat">
+                <div class="content">
+                   <span>explore, discover, travel</span>
+                   <h3>travel around the world</h3>
+                   <a href="package.php" class="btn">discover more</a> 
+                </div>
+            </div>
+
+            <div class="slide" style = "background: url('image/zoro-classic (6).jpg') no-repeat">
+                <div class="content">
+                   <span>explore, discover, travel</span>
+                   <h3>discover the new places</h3>
+                   <a href="package.php" class="btn">discover more</a> 
+                </div>
+            </div>
+
+            <div class="slide" style = "background: url('image/zoro-classic (32).jpg') no-repeat">
+                <div class="content">
+                   <span>explore, discover, travel</span>
+                   <h3>Make it worth your while</h3>
+                   <a href="package.php" class="btn">discover more</a> 
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Home section ends  -->
+
+<!-- services section starts  -->
+<section class="services">
+    <h1 class="heading-title"> our services</h1>
+
+    <div class="box-container">
+
+        <div class="class-box">
+            <img src="image/galaxy.jpg" alt="Galactic image 1">
+            <h3>Adventure</h3>
+        </div>
+
+        <div class="class-box">
+            <img src="image/darkearth.jpg" alt="Galactic image 2">
+            <h3>Mystery</h3>
+        </div>
+
+        <div class="class-box">
+            <img src="image/neutron.jpg" alt="Galactic image 3">
+            <h3>Discovery</h3>
+        </div>
+
+        <div class="class-box">
+            <img src="image/sea.jpg" alt="Galactic image 4">
+            <h3>Nature</h3>
+        </div>
+
+        <div class="class-box">
+            <img src="image/sybaris.jpg" alt="Galactic image 5">
+            <h3>Luxury</h3>
+        </div>
+
+        <div class="class-box">
+            <img src="image/under.jpeg" alt="Galactic image 6">
+            <h3>Dive deeper</h3>
+        </div>
+    </div>
+</section>
+<!-- services section ends  -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+    <div class="box-container">
+        <div class="class-box">
+
+        <h3>quick links</h3>
+        <a href="home.php"><i class="fas fa-angle-right"></i>Home</a>
+        <a href="about.php"><i class="fas fa-angle-right"></i>About</a>
+        <a href="package.php"><i class="fas fa-angle-right"></i>Package</a>
+        <a href="book.php"><i class="fas fa-angle-right"></i>Book</a>
+
+
+        </div>
+
+        <div class="class-box">
+
+        <h3>extra links</h3>
+        <a href="#"><i class="fas fa-angle-right"></i>Ask Questions</a>
+        <a href="#"><i class="fas fa-angle-right"></i>About Us</a>
+        <a href="#"><i class="fas fa-angle-right"></i>Privacy policy</a>
+        <a href="#"><i class="fas fa-angle-right"></i>Terms of Use</a>
+
+
+        </div>
+
+        <div class="class-box">
+
+        <h3>Contact Info</h3>
+        <a href="#"><i class="fas fa-phone"></i>+25474545454</a>
+        <a href="#"><i class="fas fa-phone"></i>+25417777117</a>
+        <a href="#"><i class="fas fa-envelope"></i>lassomoses@gmail.com</a>
+        <a href="#"><i class="fas fa-map"></i>Nairobi, Kenya 00100</a>
+
+
+        </div>
+
+        <div class="class-box">
+
+        <h3>Follow Us</h3>
+        <a href="#"><i class="fab fa-twitter"></i> Twitter</a>
+        <a href="#"><i class="fab fa-facebook"></i> Facebook</a>
+        <a href="#"><i class="fab fa-instagram"></i> Instagram</a>
+        <a href="#"><i class="fab fa-linkedin"></i> LinkedIn</a>
+
+
+        </div>
+
+    </div>
+
+    <div class="credit">created by <span> Mr.Lasso Moses </span> | all rights reserved.</div>
+
+</section>
+<!-- footer section ends  -->
+
+
+
+
+
+
+
+
+
+
+<!-- custom-js file  -->
+    <script src = "script.js"></script>
+</body>
+</html>
